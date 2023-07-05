232e1d53067abbef7f36d4359b50c2704b14d299
neverix <nev@neverix.io>
2023-07-06 01:24:26 +0400

    WD p2

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ parser.add_argument('--use_wandb', type=int, required=False, default=0)
 parser.add_argument('--save_step', type=int, required=False, default=None)
 parser.add_argument('--no_label', type=int, required=False, default=0)
 parser.add_argument('--tune_text', type=int, required=False, default=0)
+parser.add_argument('--weight_decay', type=float, required=False, default=1e-5)
 args = parser.parse_args()
 
 train(
@@ -21,5 +22,6 @@ train(
     use_wandb=args.use_wandb,
     save_step=args.save_step,
     no_label=args.no_label,
-    tune_text=args.tune_text
+    tune_text=args.tune_text,
+    weight_decay=args.weight_decay
 )
